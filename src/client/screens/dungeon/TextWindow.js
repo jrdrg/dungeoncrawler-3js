e6b575f44abcce6d805e41f58bce91f2debb3000
J R <JR@192>
2015-04-10 19:31:09 -0400

    Added images

---
+++ src/client/screens/dungeon/TextWindow.js
@@ -0,0 +1,19 @@
+/**
+ * Created by john on 3/29/15.
+ */
+
+
+'use strict';
+
+var exp = module.exports = {};
+
+exp.activeWindow = null;
+exp.TextWindow = TextWindow;
+
+
+var images = require('../../images'),
+    text = require('../../text');
+
+function TextWindow(text) {
+
+}
